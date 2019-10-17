c835ba3ab8f1d4855554e140cfd88f5564955125
john kevin paunel <johnkevinpaunel@gmail.com>
2019-10-17 17:46:51 +0800

    first commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+
+        $user->firstname = "John Kevin";
+        $user->middlename = "Pama";
+        $user->lastname = "Paunel";
+        $user->username = "kevinpauneljohn";
+        $user->email = "johnkevinpaunel@gmail.com";
+        $user->password = bcrypt("123");
+        $user->active = 0;
+
+        $user->save();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Route::get('admin', function () {
     return view('admin_template');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
